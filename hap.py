44bfce3bfad01bdb6cf7157968d7124f909baf72
mango <adamtamers@outlook.com>
2021-01-21 09:49:49 +0400

    Update hap.py

--- hap.py
+++ hap.py
@@ -4,16 +4,16 @@ from datetime import datetime
 while True:
     time.sleep(6)
     now = datetime.now()
-    f = open("BurningThroughTheSky.txt", 'r')
+    f = open("Script.txt", 'r')
     for word in f:
         time.sleep(0.50)
         payload = {
             'content': word
             }
         header = {
-            'authorization': 'mfa.QkVQx6kbH4XK5iE6is10rDSh-ljE2vDrsACkVm5AD_3e06YsCwXnITrGfQnQAalTUMFiBnsqgjqsKAnHA-a1'
+            'authorization': ''
             }
-        r = requests.post("https://discord.com/api/v8/channels/796057030417842176/messages",
+        r = requests.post("https://discord.com/api/v8/channels/(CHANNELID)/messages",
                           data=payload, headers=header)
         time.sleep(6)
 # https://discord.com/api/v8/channels/796057030417842176/messages
