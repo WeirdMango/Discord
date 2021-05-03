ce0387be87b699088e75b1dae9f89f18633af8d3
mango <adamtamers@outlook.com>
2021-05-03 17:04:27 +0400

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,9 +1,8 @@
-# THIS WORK IS UNDER THE NO LICENSE
+# THIS WORK IS UNDER THE NO LICENSE, refer to the License file to see the terms that you agree to if you use this.
 
 import requests
 import time
 while True:
-    time.sleep(6)
     f = open("Script.txt", 'r')
     for word in f:
         time.sleep(0.50)
@@ -16,3 +15,5 @@ while True:
         r = requests.post("https://discord.com/api/v8/channels/(INSERT CHANNELID)/messages",
                           data=payload, headers=header)
         time.sleep(6)
+
+        # MADE BY MANGOESS
